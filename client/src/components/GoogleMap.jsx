3a3bf872c4dc6c3bd35ab042ad025ff324621295
Alyzen Jeraj <38901318+alyzenjeraj@users.noreply.github.com>
2022-07-05 23:10:56 -0600

    Started maps integration

---
+++ client/src/components/GoogleMap.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+
+
+const GoogleMap = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default GoogleMap
